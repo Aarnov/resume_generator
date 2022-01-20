9bfa87fe2011cc46dae5189d52239bd1f2ee74f7
Aarnov <aarnovadhikari123@gmail.com>
2022-01-20 23:00:31 +0545

    added

---
+++ main_page.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+<!--navigation bar-->
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <div class="container">
+        <a href="#" class="navbar-brand">Cover letter Generator</a>
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a href="cover_letter" class="nav-link" >Sample</a>
+                </li>
+                <li class="nav-item">
+                    <a href="cover_letter" class="nav-link" >Make your own</a>
+                </li>
+                <li class="nav-item">
+                    <a href="logout.php" class="nav-link">Sign Out</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+</nav>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ verify.php
@@ -0,0 +1,75 @@
+<?php
+require_once "config.php";
+session_start();
+if(!isset($_SESSION["to_email"])){
+    echo "session not set";
+}
+$sql = "INSERT INTO users (verification_code) VALUES (?)";
+$stmt=mysqli_prepare($conn,$sql);
+if($stmt){
+    mysqli_stmt_bind_param($stmt,"i",$verification_code);
+    $verification_code=$_POST['code'];
+if(mysqli_stmt_execute($stmt)){
+    echo"success";
+}else{
+    echo"no success";
+}
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>Verify Page</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <style>
+        .modal-header{
+            background: black;
+            color: #fff;
+
+        }
+    </style>
+</head>
+<body>
+<h1>test</h1>
+<h1>test2</h1>
+<div container>
+    <div class="container p-5 my-5">
+
+    <div class="container p-5 my-5 bg-dark text-white">
+
+    <div class="container p-5 my-auto bg-secondary text-white">
+    <h4><?php echo"An email has been successfully sent to ". $_SESSION["to_email"]?></h4>
+    <h4> Click here to verify:</h4>
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">Verify</button>
+<div class="modal" id="myModal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header" text-center bg-primary text-white w-1000>
+                <h5 modal-title >Verify</h5>
+            </div>
+            <div class="modal-body">
+                <form action="verify.php method="post">
+                    <div>
+                    <label for="code" class="form-label text-dark"> Enter the verification code</label>
+                        <input type="number" name="code" class="form-control" id="code"></br>
+
+                </div>
+                    <button type="submit" class="btn btn-primary w-100 align-right">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</div>
+    </div>
+    </div>
+    </div>
+</div>
+</body>
+</html>
